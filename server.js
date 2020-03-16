ead4234c792dc5f2da68339d708f3150acf9d222
Glitch (hello-express) <none>
2020-03-16 02:46:09 +0000

    💃🍚 Checkpoint ./server.js:12994562/637

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
@@ -19,6 +20,12 @@ const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('Ready!');
+    client.user.setPresence({
+        game: {
+            name: 'Use "sans help" for help',
+            status: 'online'
+        }
+    });
 });
 
 client.login(token);
@@ -34,5 +41,11 @@ client.on('message', message => {
         const sanscar = new Discord.MessageAttachment('https://cdn.glitch.com/dbb9f570-9735-4542-ac26-1069d41fa06a%2Fsanscar.jpg?v=1584324797279');
 
         message.channel.send(sanscar);
+    }
+  
+  if (message.content.toLowerCase().includes('bring me the girl')) {
+        const bringMeTheGirl = new Discord.MessageAttachment('');
+
+        message.channel.send(bringMeTheGirl);
     }
 });
