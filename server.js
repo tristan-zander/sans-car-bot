15fe76bb6045e14a10ac2932111d27d3a98a26a4
Glitch (hello-express) <none>
2020-03-16 01:59:09 +0000

    🎌🐘 Checkpoint ./package.json:12994562/330 ./config.json:12994562/235 ./server.js:12994562/541

--- server.js
+++ server.js
@@ -14,3 +14,29 @@ app.listen(process.env.PORT);
 setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
+
+const Discord = require('discord.js');
+const {prefix, token} = require('./config.json');
+
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login(token);
+
+client.on('message', message => {
+    
+    if (message.content.startsWith(prefix)) {
+        console.log('command sent');
+        // do something
+    }
+
+    if (message.content.includes('sans car')) {
+        const sanscar = new Discord.MessageAttachment('./Media/sanscar.jpg');
+
+        message.channel.send(sanscar);
+    }
+});
